e105144ccd3a9388ffa5f1ad474dd25b25d692a0
Comsmelo <comsman@teamsz.xyz>
2017-03-20 20:30:59 +0800

    ifstream, not fix and fake!

--- Book_Infomation.cpp
+++ Book_Infomation.cpp
@@ -7,31 +7,35 @@
 #include<vector>
 #include<algorithm>
 #include<cstdio>
+#include<fstream> 
 using namespace std;
-
-const int maxn = 20;
-map<string, int> BookType;//图书类型映射
-map<string, int> BookPublish;//图书出版商映射 
 
 typedef struct Book_Info
 {
 	string ID;//图书检索码  XX(图书类型）XXX(出版社)XXX(出版社书序号)XXX(图书流水号) 
 	string Name;//图书名称 
 	string Publish;//出版社名称 
-	string Auther;//作者名称 
+	string Author;//作者名称 
 	string Type;//图书类型 
 	int State;//是否可借阅的状态 ,1为可借阅 
 	int Appointment_Num;//图书预约数量 
 	string Last_Borrow;//最近借书者 
 	string Intro;
+	int Reservations;
  }Book_Info;
  
 typedef struct Book
 {
 	vector<Book> Info;
 	int Book_Num;
+	string ISBN;//书籍ID除去后三位流水号 
 }Book;
- 
+
+const int maxn = 20;
+map<string, int> BookType;//图书类型映射
+map<string, int> BookPublish;//图书出版商映射 
+vector<Book> All_Book;
+
 class Book_Management
 {
 	private:
@@ -39,6 +43,7 @@ class Book_Management
 	public:
 		Book_Management();
 		void Book_Init(); 
+		string Code_Form(string Type, string Publish, string Name);//书籍编码生成 
 		void Book_Purchase(Book New_Book);//图书购入 
 		void Book_Remove(Book Old_Book);//图书清理 
 		void FindBookById(string ID);//按ID查找图书 
@@ -49,12 +54,46 @@ class Book_Management
 
 Book_Management::Book_Init()
 {
-	 
+	string information;
+	string ISBN,lastISBN;
+	Book book;
+	Book::iterator it;
+	Book_Info book_info;
+	ifstream OpenFile("Book_Info.txt", ios::in);//打开数据库文件
+	while(OpenFile.getline(information))
+	{
+		stringstream ss(information);
+		ss>>book_info.ID;
+		ss>>book_info.Name;
+		ss>>book_info.Author;
+		ss>>book_info.Publish; 
+		ss>>book_info.Intro;
+		ss>>book_info.State;
+		ss>>book_info.Last_Borrow;
+		ss>>book_info.Reservations;
+		for(int i=0; i<8; i++)
+			ISBN[i] = book_info.ID[i];
+		if(ISBN==lastISBN)
+		{
+			it = All_Book.end();
+			it->Book_Num++;
+			it->Info.push_back(book_info);
+		}
+		else
+		{
+			book.ISBN = ISBN;
+			book.Info.push_back(book_info);
+			book.Book_Num++;
+			All_Book.push_back(book);
+		}
+		lastISBN = ISBN;
+		ss.clear();
+	}
  } 
  
  Book_Management::Book_Purchase()
  {
- 	string Name, Publish, Auther, Type, Intro;
+ 	string Name, Publish, Author, Type, Intro;
 	int NBook; 
  	cout<<"请输入买入图书的名称："; 
  	cin>>Name;
@@ -65,7 +104,7 @@ Book_Management::Book_Init()
 	cout<<"请输入该图书的购入数量" ;
 	cin>>NBook;
 	cout<<"请输入该图书的简介";
-	cin>>Intro； 
+	cin>>Intro; 
   } 
  
  
